a094d4012512883d7ebd1ec49ec6f209bed8e630
Thikamphon Rabaibpho <156671933+thikamphon@users.noreply.github.com>
2025-06-24 10:30:32 +0700

    Update Portfolio.vue
    Fixed text color and shadow color.

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -8,16 +8,16 @@ const dataStore = useDataStore()
         class="bg-[var(--color-tone-obj)] content grid justify-between grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-3/4 h-3/4 overflow-x-hidden overflow-y-auto pointer-events-auto">
         <div v-for="(item, index) in dataStore.projects">
             <div class="rounded-lg card-hover  cursor-pointer card 
-            h-fit w-fit max-w-90 shadow-md shadow-gray-800" :key="index">
+            h-fit w-fit max-w-90 shadow-md shadow-gray-600" :key="index">
                 <a class="rounded-lg bg-white" :href="item.link" target="_blank">
                     <figure class="object-cover object-top h-48 md:h-30 lg:h-47 w-full">
                         <img :src="item.imageUrl" :alt="`overview of project: ` + item.title" />
                     </figure>
                     <div class="card-body">
-                        <h2 class="card-title">
+                        <h2 class="card-title text-black">
                             {{ item.title }}
                         </h2>
-                        <p class="line-clamp-2">{{ item.about }}</p>
+                        <p class="line-clamp-2 text-black">{{ item.about }}</p>
                         <div class="card-actions font-light line-clamp-1">
                             <div v-for="(tag, index) in item.tags" :key="index" class="mx-1 badge badge-warning">
                                 {{ tag }}
