31153d0d448b8b75188b58c56a05d3b766058b4a
thikamphon <thikamphon.pang@gmail.com>
2025-07-17 19:14:57 +0700

    edit path

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Portfolio from '../views/Portfolio.vue'
+import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
+// const routes = [
+//     { path: '/', name: 'home', component: () => import('../views/Home.vue') },
+//     { path: '/portfolio', name: 'portfolio', component: () => import('../views/Portfolio.vue') },
+//     { path: '/contact', name: 'contact', component: () => import('../views/Contact.vue') },
+// ]
 
-const routes = [
-    { path: '/', name: 'home', component: () => import('../views/Home.vue') },
-    { path: '/portfolio', name: 'portfolio', component: () => import('../views/Portfolio.vue') },
-    { path: '/contact', name: 'contact', component: () => import('../views/Contact.vue') },
-]
+// const router = createRouter({
+//     history: createMemoryHistory(),
+//     routes,
+// })
 
+// router/index.js
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
+    history: createWebHashHistory('/zuzooom.o/'),
+    routes: [
+        { path: '/', name: 'home', component: Home },
+        { path: '/portfolio', name: 'portfolio', component: Portfolio },
+        { path: '/contact', name: 'contact', component: Contact },
+    ],
 })
 
 export default router
